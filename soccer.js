8f9f5a41214eaebc0e516e34f29d1287198a9b64
BohdanPotapchuk <baaaagdan4444@gmail.com>
2022-10-11 23:24:24 +0300

    first commit

--- soccer.js
+++ soccer.js
@@ -1,22 +1,19 @@
-// let field = document.querySelector('.field');
 let draggable = document.querySelectorAll('.draggable');
-// let hero = document.querySelectorAll('hero');
 
 document.body.onmousedown = function (event) {
   let shiftX = event.clientX - document.body.getBoundingClientRect().left;
   let shiftY = event.clientY - document.body.getBoundingClientRect().top;
-  // console.log(1)
+
   for (let drag of draggable) {
     if (event.target.id == drag.id) {
       console.log(drag.id)
       let draggableInside = event.target;
       draggableInside.style.position = 'relative';
       draggableInside.style.zIndex = 1000;
-      // document.body.append(hero);
 
       // moveAt(event.pageX, event.pageY);
 
-      // переносит мяч на координаты (pageX, pageY),
+      // переносит героя на координаты (pageX, pageY),
       // дополнительно учитывая изначальный сдвиг относительно указателя мыши
       function moveAt(pageX, pageY) {
         console.log(event.target.id)
@@ -34,7 +31,6 @@ document.body.onmousedown = function (event) {
       // отпустить мяч, удалить ненужные обработчики
       draggableInside.onmouseup = function () {
         document.removeEventListener('mousemove', onMouseMove);
-        // ball.onmouseup = null;
       };
 
     }
